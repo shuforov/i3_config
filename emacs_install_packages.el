e338d6c48a01082b882049234630c0d8523d1ee2
shuforov <shuforov@gmail.com>
2023-10-11 13:56:02 +0300

    test

--- emacs_install_packages.el
+++ emacs_install_packages.el
@@ -1,22 +1,9 @@
 (require 'package)
 
-; find package information from following archives
 (setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
-			 ("MELPA Stable" . "https://stable.melpa.org/packages/")))
-
-;; (package-initialize)
-
-;; (mapcar (lambda (package)
-;;           ; install package if not already installed
-;;           (unless (package-installed-p package)
-;;             (package-install package)))
-
-;;         ; list of packages to be installed
-;;         '(yasnippet
-;; 	  magit
-;;           flycheck))
+			 ("melpa" . "http://melpa.milkbox.net/packages/")))
 
 (package-initialize)
 (package-refresh-contents)
-(mapc 'package-install '(magit flycheck))
+(mapc 'package-install '(magit flycheck dumb-jump))
 ;;; emacs_install_packages.el ends here
